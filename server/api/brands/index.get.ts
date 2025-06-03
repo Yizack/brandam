8b6b2923e97fa6774931f07c5741bf186cd9c59c
Yizack Rangel <yizackr@gmail.com>
2025-06-03 11:15:21 -0500

    refactor: improve brands query

--- server/api/brands/index.get.ts
+++ server/api/brands/index.get.ts
@@ -2,7 +2,9 @@ export default defineEventHandler(async (event) => {
   const { user } = await requireUserSession(event);
 
   const DB = useDB();
-
+  const members = DB.select({ value: count() }).from(tables.members).where(eq(tables.members.brandId, tables.brands.id));
+  const assets = DB.select({ value: count() }).from(tables.assets).where(eq(tables.assets.brandId, tables.brands.id));
+  // Get all brands with counts in a single query
   const brands = await DB.select({
     id: tables.brands.id,
     name: tables.brands.name,
@@ -10,14 +12,11 @@ export default defineEventHandler(async (event) => {
     description: tables.brands.description,
     updatedAt: tables.brands.updatedAt,
     createdAt: tables.brands.createdAt,
-    members: count(tables.members.id).as("members"),
-    assets: count(tables.assets.id).as("assets")
+    members: sql<number>`${members}`,
+    assets: sql<number>`${assets}`
   }).from(tables.brands)
     .innerJoin(tables.members, eq(tables.members.brandId, tables.brands.id))
-    .leftJoin(tables.assets, eq(tables.assets.brandId, tables.brands.id))
-    .where(eq(tables.members.userId, user.id))
-    .groupBy(tables.brands.id)
-    .all();
+    .where(eq(tables.members.userId, user.id)).all();
 
   return brands;
 });
