ad3ee5fdd05dcb06419d08fb6ea8fc2ded88d5e3
Yizack Rangel <yizackr@gmail.com>
2025-11-06 22:52:23 -0500

    fix: teleport admin toolbar to `#teleports`

--- app/components/AdminToolbar.vue
+++ app/components/AdminToolbar.vue
@@ -1,16 +1,18 @@
 <template>
-  <div :style="{ height: '48px' }">
-    <div class="fixed w-full bottom-0 z-50 light:bg-slate-900/80 dark:bg-slate-950/80 text-white backdrop-blur-sm">
-      <UContainer class="flex items-center justify-between gap-1 py-2 px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center gap-2">
-          <Icon name="lucide:shield-check" />
-          <span class="font-medium">Admin Mode</span>
-        </div>
-        <div class="flex items-center gap-3">
-          <ToolbarBrand />
-          <ToolbarAssets />
-        </div>
-      </UContainer>
+  <Teleport to="#teleports">
+    <div class="h-14">
+      <div class="fixed w-full bottom-0 z-50 light:bg-slate-900/80 dark:bg-slate-950/80 text-white backdrop-blur-sm">
+        <UContainer class="flex items-center justify-between gap-1 py-3 px-4 sm:px-6 lg:px-8">
+          <div class="flex items-center gap-2">
+            <Icon name="lucide:shield-check" size="1.3rem" />
+            <span class="font-bold text-lg">Admin Mode</span>
+          </div>
+          <div class="flex items-center gap-3">
+            <ToolbarBrand />
+            <ToolbarAssets />
+          </div>
+        </UContainer>
+      </div>
     </div>
-  </div>
+  </Teleport>
 </template>
