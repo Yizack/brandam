940a6878863de834586268e59816a1f1b6e13461
Yizack Rangel <yizackr@gmail.com>
2025-11-08 15:34:01 -0500

    chore: remove z-index from BrandsToolbar

--- app/components/brands/BrandsToolbar.client.vue
+++ app/components/brands/BrandsToolbar.client.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport defer to="#teleports">
     <div class="h-14">
-      <div class="fixed w-full bottom-0 z-50 light:bg-slate-900/80 dark:bg-slate-950/80 text-white backdrop-blur-sm">
+      <div class="fixed w-full bottom-0 light:bg-slate-900/80 dark:bg-slate-950/80 text-white backdrop-blur-sm">
         <UContainer class="flex items-center justify-between gap-1 py-3 px-4 sm:px-6 lg:px-8">
           <div class="flex items-center gap-2">
             <Icon name="lucide:shield-check" size="1.3rem" />
