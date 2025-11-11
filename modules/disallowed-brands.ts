ccd6409bb6f7e9d1b3f8193f87eb48daff792e86
Yizack Rangel <yizackr@gmail.com>
2025-11-11 04:07:13 -0500

    chore(disallowed-brands): ensure pages extend runs once

--- modules/disallowed-brands.ts
+++ modules/disallowed-brands.ts
@@ -1,14 +1,14 @@
 import { writeFile } from "node:fs/promises";
 import { join } from "pathe";
 import { loadFile } from "magicast";
-import { defineNuxtModule } from "nuxt/kit";
+import { defineNuxtModule, extendPages } from "nuxt/kit";
 
 export default defineNuxtModule({
   meta: {
     name: "disallowed-brands"
   },
   hooks: {
-    "pages:extend": async (pages) => {
+    ready: () => extendPages(async (pages) => {
       const pageNames = new Set(pages.map(page => page.name?.split("-")[0]));
 
       const filePath = join("server", "utils", "constants.ts");
@@ -27,6 +27,6 @@ export default defineNuxtModule({
       const generated = mod.generate({ lineTerminator: eol });
       const code = generated.code.endsWith(eol) ? generated.code : generated.code + eol;
       await writeFile(filePath, code, "utf-8");
-    }
+    })
   }
 });
