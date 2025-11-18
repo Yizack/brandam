f379bc73dcc9ab368d6e534b7fb2852ef43aedab
Yizack Rangel <yizackr@gmail.com>
2025-11-18 03:39:02 -0500

    feat: add component for text asset preview

---
+++ app/components/preview/PreviewTXT.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  uuid: string;
+  preview?: boolean;
+}>();
+
+const { data: text } = await useFetch(getAssetURL(props.uuid), {
+  responseType: "text",
+  key: `txt:${props.uuid}`,
+  getCachedData: (key, nuxtApp) => nuxtApp.payload.data[key]
+});
+</script>
+
+<template>
+  <div class="size-full p-3">
+    <pre :class="preview ? 'line-clamp-5' : 'h-98 overflow-auto'">{{ text }}</pre>
+  </div>
+</template>
